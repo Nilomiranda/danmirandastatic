80e64be8924ea35452976a3e0f0ee2b6307f8533
Danilo Miranda <danilomiranda@Danilos-MacBook-Pro.local>
2019-01-02 23:22:22 -0200

    Tests

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ class BlogIndex extends React.Component {
           const title = node.frontmatter.title || node.fields.slug
           return (
             <div key={node.fields.slug}>
-              <h3
+              <h1
                 style={{
                   marginBottom: rhythm(1 / 4),
                 }}
@@ -31,8 +31,8 @@ class BlogIndex extends React.Component {
                 <Link style={{ boxShadow: `none`, color: '#20bfb1' }} to={node.fields.slug}>
                   {title}
                 </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
+              </h1>
+              <small>{node.frontmatter.date} ---->>>>> </small>
               <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
             </div>
           )
