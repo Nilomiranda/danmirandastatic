5e6cfbf630e6e7dc9508d0c3e8d527f8c6ba739a
Danilo Miranda <danilomiranda@Danilos-MacBook-Pro.local>
2019-01-05 18:05:10 -0200

    Changed font style for texts and titles

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#20bfb1`,
         display: `minimal-ui`,
         icon: `content/assets/gatsby-icon.png`,
       },
